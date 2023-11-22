f75c913e174aeedee6618d3ed25d363ee000a419
bekesi142 <72067150+bekesi142@users.noreply.github.com>
2023-11-22 09:18:04 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+
+List<string> dobasok = new List<string>();
+List<string> osvenyek = new List<string>();
+
+StreamReader sr = new StreamReader("dobasok.txt");
+while (!sr.EndOfStream)
+{
+    string[] dob = sr.ReadLine().Split(" ");
+	for (int i = 0; i < dob.Length; i++)
+	{
+		dobasok.Add(dob[i]);
+	}
+}
+sr.Close();
+
+StreamReader sr2 = new StreamReader("osvenyek.txt");
+while (!sr2.EndOfStream)
+{
+    sr2.ReadLine();
+	osvenyek.Add(sr2.ReadLine());
+    
+}
+sr2.Close();
+
+
+
+
+
+Console.WriteLine("2. feladat:"  );
+Console.WriteLine("A dobások száma: " + dobasok.Count);
+Console.WriteLine("Az ösvények száma:  " + osvenyek.Count);
+
+//Megjegyzés: késés miatt jutottam idáig.
